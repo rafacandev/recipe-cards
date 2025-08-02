044394bc698f6a3e76ec44a9c9e8a17c0de6972d
Rafael Santos <rafacandev@gmail.com>
2025-08-02 18:27:36 -0400

    style: refine recipe card

--- src/components/RecipesPage/RecipesPage.jsx
+++ src/components/RecipesPage/RecipesPage.jsx
@@ -4,13 +4,9 @@ import recipes from "./recipes.json"
 const imageSrc = (image = "") => "./recipes/" + image
 
 const Card = ({ name = "", image = "", ingredients = [], directions = [] }) => (
-  <div class="card card-border w-96 bg-base-100 card-md shadow-sm">
+  <div class="card card-border max-w-96 bg-base-100 card-md shadow-sm">
     <figure>
-      <img
-        class="h-52 w-full object-cover"
-        src={imageSrc(image)}
-        alt={name}
-      />
+      <img class="h-52 w-full object-cover" src={imageSrc(image)} alt={name} />
     </figure>
     <div class="card-body">
       <h2 class="card-title">{name}</h2>
@@ -19,7 +15,7 @@ const Card = ({ name = "", image = "", ingredients = [], directions = [] }) => (
         <For each={ingredients}>{i => <li>{i}</li>}</For>
       </ul>
       <p>Directions:</p>
-      <ul class="list-[square] list-inside">
+      <ul class="list-[square] list-inside space-y-2">
         <For each={directions}>{i => <li>{i}</li>}</For>
       </ul>
       <div class="justify-end card-actions">
