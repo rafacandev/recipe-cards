200b55fe7f40036f1ef31ee123edda144494b053
Rafael Santos <rafacandev@gmail.com>
2025-08-01 21:59:30 -0400

    feat: add new RecipesPage

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./Header"
 import { Footer } from "./Footer"
+import { RecipesPage } from "../RecipesPage/RecipesPage"
 
 export const Layout = () => (
   <div class="flex flex-col h-screen w-full">
@@ -11,7 +12,7 @@ export const Layout = () => (
 
     <div class="flex grow px-1 justify-center bg-base-200">
       <div class="w-full max-w-4xl h-full bg-base-100 sm:px-1 lg:px-3">
-        <div>Contente goes here</div>
+        <RecipesPage />
       </div>
     </div>
 
---
+++ src/components/RecipesPage/RecipesPage.jsx
@@ -0,0 +1,3 @@
+export const RecipesPage = () => {
+  return <>Recipes</>
+}
