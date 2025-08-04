71f4e49c33c1a5993f9a67033f8fb2a4a77c4ed6
Rafael Santos <rafacandev@gmail.com>
2025-08-04 00:03:22 -0400

    refactor: extract Card component

--- src/components/RecipesPage/RecipesPage.jsx
+++ src/components/RecipesPage/RecipesPage.jsx
@@ -1,29 +1,6 @@
 import { For } from "solid-js"
 import recipes from "./recipes.json"
-
-const imageSrc = (image = "") => "./recipes/" + image
-
-const Card = ({ name = "", image = "", ingredients = [], directions = [] }) => (
-  <div class="card card-border max-w-[440px] bg-base-100 card-md shadow-sm">
-    <figure>
-      <img class="h-48 w-full object-cover" src={imageSrc(image)} alt={name} />
-    </figure>
-    <div class="card-body">
-      <h2 class="card-title">{name}</h2>
-      <h3 class="text-slate-700 font-medium">Ingredients</h3>
-      <ul class="list-[square] list-inside">
-        <For each={ingredients}>{i => <li>{i}</li>}</For>
-      </ul>
-      <h3 class="text-slate-700 font-medium">Directions</h3>
-      <ul class="list-[square] list-inside space-y-2">
-        <For each={directions}>{i => <li>{i}</li>}</For>
-      </ul>
-      <div class="justify-end card-actions">
-        <button class="btn btn-primary">Select</button>
-      </div>
-    </div>
-  </div>
-)
+import { Card } from "./Card"
 
 export const RecipesPage = () => {
   return (
