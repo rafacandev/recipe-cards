43cf114751f4d3e1114843dc8397c571616d8d1e
Rafael Santos <rafacandev@gmail.com>
2025-08-07 23:51:50 -0400

    fix(routing): correct component prop usage in Route

--- src/components/RecipesPage/MiniCard.jsx
+++ src/components/RecipesPage/MiniCard.jsx
@@ -1,13 +1,23 @@
 import { CardImage } from "./CardImage"
+import { useNavigate } from "@solidjs/router"
 
-export const MiniCard = ({ name = "", image = "" }) => (
-  <div class="card card-border bg-white card-md shadow-sm">
-    <CardImage image={image} name={name} />
-    <div class="card-body">
-      <h2 class="card-title">{name}</h2>
-      <div class="justify-end card-actions">
-        <button class="btn btn-primary">Select</button>
+export const MiniCard = ({ recipeId = "", name = "", image = "" }) => {
+  const navigate = useNavigate()
+  const handleOnClick = () => {
+    navigate(`/recipe/${recipeId}`)
+  }
+
+  return (
+    <div class="card card-border bg-white card-md shadow-sm">
+      <CardImage image={image} name={name} />
+      <div class="card-body">
+        <h2 class="card-title">{name}</h2>
+        <div class="justify-end card-actions">
+          <button class="btn btn-primary" onClick={handleOnClick}>
+            Select
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
--- src/routing.jsx
+++ src/routing.jsx
@@ -9,7 +9,7 @@ const testR = () => <div>testing route</div>
 
 export const AppRoutes = () => (
   <Router base={baseUrl} root={Layout}>
-    <Route path="/" component={<RecipesPage />} />
+    <Route path="/" component={RecipesPage} />
     <Route path="/recipe/:recipeId" component={RecipePage} />
     <Route path="/test" component={testR} />
     <Route path="*404" component={NotFound} />
