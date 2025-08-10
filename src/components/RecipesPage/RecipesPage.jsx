4a4d529d0738c54434aad5aa5e2d99b0676281f4
Rafael Santos <rafacandev@gmail.com>
2025-08-09 22:49:24 -0400

    style: add an intro paragraph to initial page

--- src/components/RecipesPage/RecipesPage.jsx
+++ src/components/RecipesPage/RecipesPage.jsx
@@ -2,13 +2,12 @@ import { For } from "solid-js"
 import { recipes } from "./recipes"
 import { Card } from "./Card"
 
-export const RecipesPage = () => {
-  return (
-    <>
-      <h1 class="text-4xl">Recipes</h1>
-      <div class="grid grid-cols-1 sm:grid-cols-2 gap-2">
-        <For each={recipes}>{recipe => <Card {...recipe} />}</For>
-      </div>
-    </>
-  )
-}
+export const RecipesPage = () => (
+  <>
+    <h1 class="text-2xl my-4">Recipes</h1>
+    <p class="mb-6 text-center">Recipes in card format: easy to cook, print, and share.</p>
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-2">
+      <For each={recipes}>{recipe => <Card {...recipe} />}</For>
+    </div>
+  </>
+)
