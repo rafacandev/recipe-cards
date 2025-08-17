fe7983e4665a09bf4c6f5fe3fd044ef8d5dd9981
Rafael Santos <rafacandev@gmail.com>
2025-08-16 20:57:52 -0400

    feat: rebrand to recipe cards

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,7 +1,9 @@
 import { A } from "@solidjs/router"
+import { Cake } from "../../icons/Cake"
 
 export const Header = () => (
-  <A href="/" class="text-2xl text-white hover:no-underline">
+  <A href="/" class="flex gap-2 items-center text-2xl text-white hover:no-underline">
+    <Cake />
     <h1>Recipe Cards</h1>
   </A>
 )
