ae4d074fa72e5851e7d34bdaf9db82c95abf65a0
Rafael Santos <rafacandev@gmail.com>
2025-08-17 17:16:30 -0400

    feat: click on card img navigates to recipe

--- src/components/RecipesPage/Card.jsx
+++ src/components/RecipesPage/Card.jsx
@@ -10,7 +10,7 @@ export const Card = ({ recipeId = "", name = "", image = "" }) => {
 
   return (
     <div class="card card-border bg-white card-md shadow-sm">
-      <CardImage image={image} name={name} />
+      <CardImage image={image} name={name} onclick={handleOnClick} />
       <div class="card-body">
         <h2 class="card-title">{name}</h2>
         <div class="justify-end card-actions">
--- src/components/RecipesPage/CardImage.jsx
+++ src/components/RecipesPage/CardImage.jsx
@@ -2,8 +2,8 @@ import { publicResource } from "../../core/public"
 
 const imageSrc = (image = "") => publicResource(`recipes/${image}`)
 
-export const CardImage = ({ image = "", name = "" }) => (
-  <figure>
+export const CardImage = ({ image = "", name = "", onclick = () => {} }) => (
+  <figure onclick={onclick}>
     <img class="h-48 w-full object-cover" src={imageSrc(image)} alt={name} />
   </figure>
 )
