d52215da5928866ade2a8e10bf42c8b7106e4d41
Rafael Santos <rafacandev@gmail.com>
2025-08-17 20:38:23 -0400

    refactor: rename Layout.jsx to DefaultLayout.jsx
    The Layout component has been renamed to DefaultLayout to better reflect its purpose as the default layout for the application.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { Router, Route } from "@solidjs/router"
 import { RecipesPage } from "../RecipesPage/RecipesPage"
-import { Layout, FullScreenLayout } from "../Layout/Layout"
+import { DefaultLayout, FullScreenLayout } from "../Layout/DefaultLayout"
 import { NotFound } from "../NotFound/NotFound"
 import { RecipePage } from "../RecipePage/RecipePage"
 import { baseUrl } from "../../core/public"
@@ -9,7 +9,7 @@ import { PrintPage } from "../PrintPage/PrintPage"
 
 export const App = () => (
   <Router base={baseUrl}>
-    <Route path="/" component={Layout}>
+    <Route path="/" component={DefaultLayout}>
       <Route path="/" component={RecipesPage} />
       <Route path="/recipe/:recipeId" component={RecipePage} />
       <Route path="*404" component={NotFound} />
