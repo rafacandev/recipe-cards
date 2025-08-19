3b9925acfeccee6ea053b85857eb899c41d80b48
Rafael Santos <rafacandev@gmail.com>
2025-08-18 20:48:26 -0400

    feat(print): print page on mount and navigate back

--- src/components/PrintPage/PrintPage.jsx
+++ src/components/PrintPage/PrintPage.jsx
@@ -1,10 +1,17 @@
-import { useParams } from "@solidjs/router"
+import { useParams, useNavigate } from "@solidjs/router"
+import { onMount } from "solid-js"
 import { recipes } from "../RecipesPage/recipes"
 import { Card } from "../RecipePage/Card"
 
 export const PrintPage = () => {
   const params = useParams()
+  const navigate = useNavigate()
   const recipe = recipes.find(r => r.recipeId === params.recipeId) ?? null
+
+  onMount(() => {
+    window.print()
+    navigate(-1)
+  })
 
   return (
     <div class="my-4">
