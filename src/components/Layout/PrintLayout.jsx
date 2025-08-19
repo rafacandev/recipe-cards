c54e8f4857256061275803aa1b9f68f055940b5c
Rafael Santos <rafacandev@gmail.com>
2025-08-18 20:36:51 -0400

    feat(layout): set print layout context on mount

--- src/components/Layout/PrintLayout.jsx
+++ src/components/Layout/PrintLayout.jsx
@@ -1,5 +1,13 @@
-export const PrintLayout = props => (
-  <div class="flex justify-center">
-    <div class="flex grow h-[200mm] w-[250mm] justify-center">{props.children}</div>
-  </div>
-)
+import { onMount } from "solid-js"
+import { useLayout } from "../../context/LayoutContext"
+
+export const PrintLayout = props => {
+  const { setPrintLayout } = useLayout()
+  onMount(() => setPrintLayout())
+
+  return (
+    <div class="flex justify-center">
+      <div class="flex grow h-[200mm] w-[250mm] justify-center">{props.children}</div>
+    </div>
+  )
+}
