48fbad0cb0bf6f02918b789d7454f25bf03473b5
Rafael Santos <rafacandev@gmail.com>
2025-08-24 11:30:20 -0400

    debug: auto-redirects without debug buttons

--- src/components/RedirectPage/RedirectPage.jsx
+++ src/components/RedirectPage/RedirectPage.jsx
@@ -4,14 +4,12 @@ export const RedirectPage = () => {
   const [searchParams] = useSearchParams()
   const to = searchParams.to ?? "/recipes/"
   const navigate = useNavigate()
-  console.log(`navigating to: ${to}`, { searchParams })
+  console.log(`redirecting to: ${to}`)
+  navigate(to)
 
   return (
     <div>
       This will be redirected to: <code>{to}</code>
-      <button class="btn" onclick={() => navigate(to)}>
-        Redirect
-      </button>
     </div>
   )
 }
