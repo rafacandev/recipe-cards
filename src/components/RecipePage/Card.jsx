7185a30c57da45c684291c1cb4b0702de3dab2a5
Rafael Santos <rafacandev@gmail.com>
2025-08-29 20:03:48 -0400

    feat: add iframe button to recipe card

--- src/components/RecipePage/Card.jsx
+++ src/components/RecipePage/Card.jsx
@@ -1,8 +1,7 @@
-import { For, Show } from "solid-js"
+import { For, onCleanup, createSignal, createMemo } from "solid-js"
 import { publicResource, baseUrl } from "../../context/public"
 import { useNavigate } from "@solidjs/router"
 import { useLayout } from "../../context/LayoutContext"
-import { createMemo } from "solid-js"
 
 const imageSrc = (image = "") => publicResource(`recipes/${image}`)
 
@@ -20,13 +19,45 @@ const Body = ({ ingredients = [""], directions = [""] }) => (
 )
 
 export const Card = ({ recipeId = "", name = "", image = "", ingredients = [], directions = [] }) => {
+  let iframe
   const navigate = useNavigate()
   const { layout } = useLayout()
+  const [iframeDisabled, setIframeDisabled] = createSignal(false)
   const layoutCardClasses = createMemo(() => (layout() === "print" ? "border-2 border-gray-300" : "card"))
 
   const handlePrint = () => {
     window.open(`${baseUrl}/print/recipes/${recipeId}`, "print-recipe")
   }
+
+  const cleanupIframe = () => {
+    if (iframe) {
+      document.body.removeChild(iframe)
+      iframe = null
+    }
+    window.removeEventListener('message', handleIframeMessage)
+  }
+
+  const handleIframeMessage = (event) => {
+    if (event.data === 'saveCompleted') {
+      cleanupIframe()
+      setIframeDisabled(false)
+    }
+  }
+
+  const handleIframe = () => {
+    if (iframe) return
+    setIframeDisabled(true)
+    iframe = document.createElement('iframe')
+    iframe.style.display = "none" 
+    iframe.style.width = "25cm"
+    iframe.src = `${baseUrl}/save/pdf/recipes/${recipeId}`
+    window.addEventListener('message', handleIframeMessage)
+    document.body.appendChild(iframe)
+  }
+
+  onCleanup(() => {
+    cleanupIframe()
+  })
 
   return (
     <div class={`${layoutCardClasses()} card-md card-border bg-white shadow-sm`}>
@@ -42,6 +73,9 @@ export const Card = ({ recipeId = "", name = "", image = "", ingredients = [], d
           </button>
           <button class="btn btn-primary" onClick={handlePrint}>
             Print
+          </button>
+          <button class="btn btn-primary" onClick={handleIframe} disabled={iframeDisabled()}>
+            iframe
           </button>
         </div>
       </div>
