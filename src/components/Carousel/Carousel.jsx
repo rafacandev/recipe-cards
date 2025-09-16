5977f124465fb05de698d082021d486ceff18749
Rafael Santos <rafacandev@gmail.com>
2025-09-15 22:22:53 -0400

    doc: jsdoc carousel types

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,8 +1,23 @@
 import { For, Show } from "solid-js"
 
+/**
+ * @typedef {Object} CarouselItemProps
+ * @property {(r: HTMLDivElement) => void} setRef
+ * @property {string} imgSrc
+ * @property {boolean} hasPrevious
+ * @property {(position: number) => void} onPrevious
+ * @property {number} position
+ * @property {boolean} hasNext
+ * @property {(position: number) => void} onNext
+ */
+
+/**
+ * A single item in the carousel.
+ * @param {CarouselItemProps} props
+ */
 const CarouselItem = props => {
   return (
-    <div ref={r => props.onRef(r)} class="carousel-item relative w-full">
+    <div ref={r => props.setRef(r)} class="carousel-item relative w-full">
       <img src={props.imgSrc} class="w-full h-48 object-cover" />
       <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
         <Show when={props.hasPrevious} fallback={<div></div>}>
@@ -34,7 +49,7 @@ export const Carousel = ({ images = [] }) => {
             position={position()}
             hasNext={position() < items.length - 1}
             hasPrevious={position() > 0}
-            onRef={r => (item.ref = r)}
+            setRef={r => (item.ref = r)}
             imgSrc={item.img}
             onNext={handleNext}
             onPrevious={handlePrevious}
